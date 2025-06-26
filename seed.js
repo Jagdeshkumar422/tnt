bb5f830ae7904fea714aab3ef23f74a191a7c9c5
Jagdeshkumar422 <jk9876211@gmail.com>
2025-06-26 08:15:16 +0500

    dffgf

--- seed.js
+++ seed.js
@@ -1,22 +1,49 @@
 const mongoose = require('mongoose');
-const axios = require('axios');
-const Nft = require('./models/Nft'); // Adjust path if needed
+const Nft = require('./models/Nft'); // Adjust path
 
-const MONGODB_URI = 'mongodb+srv://treasusrexnft:admin@cluster0.j1yqv8y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // Replace with your DB
+const MONGODB_URI = 'mongodb+srv://treasusrexnft:admin@cluster0.j1yqv8y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-const TOTAL_NFTS = 1500; // Set between 1000 to 3000
+const TOTAL_NFTS = 1500;
 const START_PRICE = 48;
 const NFTS_PER_PRICE = 3;
+
+const customImages = [
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616950/7748166_cxhcha.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616948/Five-NFT-Challenges-1_dkbnkv.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616949/th_f5wfk5.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616948/bayc-8817-1024x1024_c9rgwm.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616948/boredape_ooqmnk.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616948/nft-monkey-4k7xu9wjiuy4say2_o8qvlb.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616948/nft-background-l8fq4jdq324hhoek_zohtgm.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616948/th_1_njxghh.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616929/boredape_1_bglvd6.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616928/61254c10-c4e2-11ec-b6eb-8947bb0d6d4e.cf_rrw36t.jpg',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616928/mutant-ape-yacht-club-nft-artwork-collection-set-unique-bored-monkey-character-nfts-variant_361671-259_sezzff.avif',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616929/bored_ape_1200x800_2022_wqwy37.webp',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616927/tpxvny1fbdv61_zhryqf.png',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616928/8cbbaf683da595be433f6342a1fab0c1_kunpe6.png',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616929/wp10537285_xtnm1i.webp',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616928/ape-007_wqtzk7.webp',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616927/bored-ape-sad-gID_7_bijk9c.png',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616929/Bored-Ape-Yacht-Club-NFT-numero-8585-_r8ttr4.webp',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616929/boredapeyachtclub_nft_image_158_diaqhi.webp',
+  'https://res.cloudinary.com/di0qbhv97/image/upload/v1750616928/wp10578770_bz8iji.webp',
+
+];
 
 const generateRandomString = (length = 10) =>
   Math.random().toString(36).substring(2, 2 + length);
 
-const generateDummyNFT = (price, index) => ({
-  name: `NFT_${price}_${index}_${generateRandomString(3)}`,
-  hash: generateRandomString(64),
-  price,
-  image: `https://api.dicebear.com/6.x/pixel-art/svg?seed=${generateRandomString(8)}`
-});
+const generateDummyNFT = (price, index) => {
+  const randomImage = customImages[Math.floor(Math.random() * customImages.length)];
+
+  return {
+    name: `NFT_${price}_${index}_${generateRandomString(3)}`,
+    hash: generateRandomString(64),
+    price,
+    image: randomImage,
+  };
+};
 
 async function seedNFTs() {
   await mongoose.connect(MONGODB_URI);
@@ -31,7 +58,7 @@ async function seedNFTs() {
       await Nft.create(nft);
       created++;
     }
-    price++; // Increase price for next group
+    price++;
   }
 
   console.log(`✅ Successfully created ${created} NFTs`);
