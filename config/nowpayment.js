7ba686ccdc526611e2cf003dc4eef373d31ae8ca
Jagdeshkumar422 <jk9876211@gmail.com>
2025-06-28 15:53:36 +0500

    dffgf

--- config/nowpayment.js
+++ config/nowpayment.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 
 // const NOWPAYMENTS_API_KEY = process.env.NOWPAYMENTS_API_KEY;
 // const NOWPAYMENTS_API_KEY = "GBEF217-3S5MGFV-JWRPNTQ-G7Y2KE4";
-const NOWPAYMENTS_API_KEY = "XBFJZXA-77P4DVW-K91BPXY-HA9DNG7";
+// const NOWPAYMENTS_API_KEY = "XBFJZXA-77P4DVW-K91BPXY-HA9DNG7";
+const NOWPAYMENTS_API_KEY = "0J3M9NP-D66M7TE-HRNCJM4-47DE55R";
 
 const nowPaymentsClient = axios.create({
   baseURL: 'https://api.nowpayments.io/v1',
